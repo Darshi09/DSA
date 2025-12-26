065ea6e1587f0ff6c6e6e6c1e2e1e6d5ee495900
Darshi09 <shahdarshi.21.imca@iict.indusuni.ac.in>
2025-12-26 10:01:51 +0530

    DSA

--- p1.js
+++ p1.js
@@ -1,4 +1,58 @@
-let a = [9, 3, 5, 6, 2, 7, 1, 4, 8, 33, 556, 88, 33, 55, 23, 11];
-for (let i = 0; i < a.length; i++) {
-  console.log(a[i]);
-}
+// arr = [1,2,3,4,5,6,7,8,9]
+
+//access the data 
+// for (let i =0;i<arr.length;i++){
+//   console.log(arr[i])
+// }
+
+// insert the data 
+// let newpos = 5
+// let newvalue = 100
+
+// for(let i= arr.length-1; i>=0 ;i--){
+//   if(i>= newpos){
+//     arr[i+1] = arr[i]
+//     if (i== newpos){
+//       arr[i] = newvalue
+//     }
+//   }
+// }
+// console.log(arr)
+
+//deletetion
+// let newpos = 3;
+
+// for (let i=newpos; i<arr.length; i++){
+//   arr[i]=arr[i+1]
+// } 
+// arr.length = arr.length-1
+// console.log(arr)
+
+
+// start
+let arr = [11,22,33,22,33,55,67,76,44,99,88]
+let newpos = 6 
+let newvalue = 1000
+
+// asscess 
+// for (let i=0 ; i<= arr.length-1 ; i++){
+//   console.log (arr[i])
+// }
+
+// insert
+// for (i= arr.length-1; i>=0; i--){
+//   if (i >= newpos){
+//     arr[i+1] = arr[i]
+//     if (i==newpos){
+//       arr[i]=newvalue
+//     }
+//   }
+// }
+// console.log(arr)
+
+//delete
+// for (i=newpos;i<arr.length; i++){
+//   arr[i] = arr[i+1]
+// }
+// arr.length = arr.length-1
+// console.log(arr)
